c8183852577e1b99382ffec8beaf741114bd4cfb
nareshpmaskeva <nareshaskeva726@gmail.com>
2026-06-09 12:29:35 +0530

    integration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,7 +23,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://krs-o77k.vercel.app"
+    ],
     credentials: true,
   })
 );
